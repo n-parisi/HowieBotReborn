3bd2d288fbecc998f771654784007d86e39b9c46
Nick Parisi <nick@nick-parisi.com>
2020-08-20 00:32:05 -0400

    Add help command

--- bot.py
+++ bot.py
@@ -5,11 +5,22 @@ from cogs.pics import Pics
 from cogs.sounds import Sounds
 
 # create bot
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', help_command=None)
 
 # attach cogs
 bot.add_cog(Sounds(bot))
 bot.add_cog(Pics(bot))
+
+
+# add help command
+@bot.command()
+async def help(ctx):
+    await ctx.send('```Available Commands: \n'
+                   '   !pic                 Share a random Howie Pic\n'
+                   '   !play                Play a random sound clip\n'
+                   '   !sounds              List all clips\n'
+                   '   !play <clip name>    Play a specific sound clip```')
+
 
 # start bot
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
