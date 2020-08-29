3d1be5b5470c398edd7df349756407512b5f142f
Nick Parisi <nickparisi@pm.me>
2020-08-29 11:54:02 -0400

    Remove scripts from version control

--- scripts/runLocal.sh
+++
@@ -1,5 +0,0 @@
-docker run -it --rm \
-  -e DISCORD_BOT_TOKEN=NzQ1NDU4ODkxMjM5NzE4OTU0.XzyEsQ.1dWaIT_m9ZMgCNuqtFurQbnOwR8 \
-  -v "$PWD":/home/app \
-  nickparisi/howiebot:latest python3 -u main.py
-
